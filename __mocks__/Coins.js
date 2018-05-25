e338cdead5b878fa996bcb05225bd022d08e57ba
Brandon Liu <brandon.slipjet@gmail.com>
2018-05-25 09:53:36 -0700

    added spec file to test folder and added gitignore/readme. Vendingmachine test functions added.

--- __mocks__/Coins.js
+++ __mocks__/Coins.js
@@ -1,7 +1,13 @@
 module.exports = {
-    nickel: 0.05,
-    dime: 0.1,
-    quarter: 0.25,
-    loonie: 1.0,
-    toonie: 2.0
+    nickel: {
+        value: 0.05,
+        quantity: 20
+    },
+    dime: {
+        value: 0.1,
+        quantity: 15
+    },
+    quarter: { value: 0.25, quantity: 15 },
+    loonie: { value: 1.0, quantity: 10 },
+    toonie: { value: 2.0, quantity: 10 }
 };
