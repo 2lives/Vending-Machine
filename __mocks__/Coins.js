4ee0d2411413ca0b29a7d97529a930e8d90f271c
Brandon Liu <brandon.slipjet@gmail.com>
2018-05-25 09:44:26 -0700

    added mock data to mock folder.

---
+++ __mocks__/Coins.js
@@ -0,0 +1,7 @@
+module.exports = {
+    nickel: 0.05,
+    dime: 0.1,
+    quarter: 0.25,
+    loonie: 1.0,
+    toonie: 2.0
+};
---
+++ __mocks__/Inventory.js
@@ -0,0 +1,32 @@
+module.exports = {
+    a1: {
+        item: 'Lamborghini',
+        price: 5.0,
+        quantity: 1
+    },
+    a2: {
+        item: 'McLaren',
+        price: 4.5,
+        quantity: 4
+    },
+    a3: {
+        item: 'Ferrari',
+        price: 6.0,
+        quantity: 2
+    },
+    b1: {
+        item: 'Ducati',
+        price: 2.0,
+        quantity: 3
+    },
+    b2: {
+        item: 'Triumph',
+        price: 2.75,
+        quantity: 1
+    },
+    b3: {
+        item: 'Yamaha',
+        price: 0.5,
+        quantity: 100
+    }
+};
