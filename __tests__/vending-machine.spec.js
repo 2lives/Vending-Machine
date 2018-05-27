8444b68f1b9a5d89adcb1461a988367043dd6040
Brandon Liu <brandon.slipjet@gmail.com>
2018-05-27 16:42:02 -0700

    dispense change tests working.

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -10,19 +10,32 @@ describe('Vending Machine', () => {
             expect(result).toEqual(inv);
         });
     });
-    describe('Restock change with invalid input (not coins)', () => {
-        describe('Given an input other than coins in the form of a single coin or multiples', () => {
-            it('Should throw an error', () => {
-                const result = () => vendingMachine.restockChange([]);
-                expect(result).toThrow();
+    describe('Restock coins', () => {
+        describe('Restock singular coin, with valid input', () => {
+            it('Should increment the quantity of specified coin, by specified amount', () => {
+                const result = vendingMachine.restockSingularCoin({
+                    coin: 'loonie',
+                    quantity: 41
+                });
+
+                expect(result).toEqual(51);
             });
         });
-    });
-    describe('Restock bulk change with valid coins', () => {
-        describe('Given a valid input for change', () => {
-            it('Should increment the change inventory by specified amount and return true', () => {
-                const result = vendingMachine.restockBulkChange(15);
-                expect(result).toEqual([35, 30, 30, 25, 25]);
+        describe('Restock change with invalid input (not coins)', () => {
+            describe('Given an input other than coins in the form of a single coin or multiples', () => {
+                it('Should throw an error', () => {
+                    const result = () => vendingMachine.restockChange([]);
+                    expect(result).toThrow();
+                });
+            });
+        });
+        describe('Restock bulk change with valid coins', () => {
+            describe('Given a valid input for change', () => {
+                it('Should increment the change inventory by specified amount and return true', () => {
+                    const result = vendingMachine.restockBulkChange(15);
+                    //loonie is 66 because of the accumulative 41 loonies added previously
+                    expect(result).toEqual([35, 30, 30, 66, 25]);
+                });
             });
         });
     });
@@ -59,6 +72,72 @@ describe('Vending Machine', () => {
                 });
                 //equals 61 because the bulk 20 inv added in the previous test passed, therefore adding another 20 to the total stock
                 expect(result).toEqual(61);
+            });
+        });
+    });
+    describe('Dispense change', () => {
+        describe('Given either an invalid input, or the input (money) is not large enough to purchase desired item', () => {
+            it('Should throw an error', () => {
+                const result = () => vendingMachine.dispenseChange(5, 2);
+                expect(result).toThrow();
+            });
+        });
+        describe('Given a valid input, where the money input is greater than the price of the item and the change is not a float', () => {
+            it('should dispense the change', () => {
+                const result = vendingMachine.dispenseChange(5, 7);
+                expect(result).toEqual({
+                    toonie: 1,
+                    loonie: 0,
+                    quarter: 0,
+                    dime: 0,
+                    nickel: 0
+                });
+            });
+        });
+        describe('Given a valid input, where the money input is greater than the price of the item and the change is a float', () => {
+            it('Should return the proper change combination', () => {
+                const result = vendingMachine.dispenseChange(5, 7.25);
+                expect(result).toEqual({
+                    toonie: 1,
+                    loonie: 0,
+                    quarter: 1,
+                    dime: 0,
+                    nickel: 0
+                });
+            });
+        });
+        describe('Given a valid input where the money input is greater than the price of the item and the change features no dollars (integers)', () => {
+            it('Should return the proper change combination', () => {
+                const result = vendingMachine.dispenseChange(5, 5.4);
+                expect(result).toEqual({
+                    toonie: 0,
+                    loonie: 0,
+                    quarter: 1,
+                    dime: 1,
+                    nickel: 1
+                });
+            });
+        });
+        describe('Given a valid input where the money input is greater than the price of the item and the change (float) is not a multiple of 5', () => {
+            it('Should return the proper change combination', () => {
+                const result = vendingMachine.dispenseChange(5, 5.42);
+                expect(result).toEqual({
+                    toonie: 0,
+                    loonie: 0,
+                    quarter: 1,
+                    dime: 1,
+                    nickel: 1
+                });
+            });
+        });
+    });
+    describe('Dispense item', () => {
+        describe('Given an invalid input (dollar amount), being an inadequate amount or something other than money', () => {
+            it('Should throw an error', () => {
+                const result = () => vendingMachine.dispenseInvItem(5, 3);
+                const result1 = () => vendingMachine.dispenseInvItem('hello');
+                expect(result).toThrow();
+                expect(result1).toThrow();
             });
         });
     });
